3ee8b60c8979ea0a8be0fb115f3a159c3d8d9466
jojo157 <carrigj@tcd.ie>
2021-11-19 19:41:37 +0000

    added data to database

--- guessapp/admin.py
+++ guessapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Roadsign
 
+class RoadsignAdmin(admin.ModelAdmin):
+    list_display = ("id","sign_name")
+
 # Register your models here.
-admin.site.register(Roadsign)
+admin.site.register(Roadsign, RoadsignAdmin)
