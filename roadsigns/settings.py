24511a6fb5038575b25db70c48968ed07ecc6cd1
jojo157 <carrigj@tcd.ie>
2021-11-20 21:08:32 +0000

    updated settings

--- roadsigns/settings.py
+++ roadsigns/settings.py
@@ -90,11 +90,17 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
