9b7201a12b0100656a036a2b888256648dca0a2e
jojo157 <carrigj@tcd.ie>
2021-11-20 21:03:30 +0000

    unifying data

--- roadsigns/settings.py
+++ roadsigns/settings.py
@@ -90,17 +90,11 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {
+
+DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+}
+
 
 
 # Password validation
